79d2c14b017ac28032333a59696f3b689554c305
Tom Clemente <underscor_e@yahoo.com>
2011-12-02 16:43:41 +0800

    Add: project source

---
+++ src/interrupt/Main.java
@@ -0,0 +1,17 @@
+package interrupt;
+import java.lang.*;
+import arjuna.JavaSim.*;
+
+public class Main
+{
+
+public static void main (String[] args)
+    {
+	MachineShop m = new MachineShop();
+	
+	m.Await();
+
+	System.exit(0);
+    }
+    
+}
